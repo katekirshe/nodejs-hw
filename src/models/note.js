fae4174ff1028e0bfe4f6d1fc80a6b8ab4cda667
Kateryna <katekirshe95@gmail.com>
2026-06-07 16:44:21 +0300

    first try

--- src/models/note.js
+++ src/models/note.js
@@ -1,5 +1,6 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
+import { TAGS } from '../constants/tags';
 
 const noteSchema = new Schema(
   {
@@ -17,8 +18,9 @@ const noteSchema = new Schema(
     tag: {
       type: String,
       required: false,
-      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+      enum: TAGS,
       default: 'Todo',
+      index: true,
     }
   },
   {
