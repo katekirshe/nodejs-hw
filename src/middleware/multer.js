5057a6ead43142c62ccab5da14095634a182a529
Kateryna <katekirshe95@gmail.com>
2026-06-21 10:10:44 +0300

    quick fix-2

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -6,13 +6,11 @@ export const upload = multer({
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-	  const allowedTypes = ['image/'];
-
-	  if (allowedTypes.includes(file.mimetype)) {
-      cb(null, true);
-	  } else {
-      cb(new Error('Only images allowed.'), false);
-	  }
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only images allowed'), false);
+  }
   },
 });
 
