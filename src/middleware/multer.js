23ff8e9ddf39f0e253a4626763e68cdb1346339c
Kateryna <katekirshe95@gmail.com>
2026-06-21 09:55:40 +0300

    hw 5

---
+++ src/middleware/multer.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+	  const allowedTypes = ['image/'];
+
+	  if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true);
+	  } else {
+      cb(new Error('Only images allowed.'), false);
+	  }
+  },
+});
+
--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from "cookie-parser";
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -21,6 +22,7 @@ app.use(cors()); // Дозволяє запити з будь-яких джер�
 app.use(cookieParser());
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 app.use(errors());
 
 // // Маршрут для тестування middleware помилки
